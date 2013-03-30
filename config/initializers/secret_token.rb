5cc5fa3468d19b3777fd9a2c1b1000f519b3e23a
Hemant V. Torsekar <your_email@example.com>
2013-03-30 18:52:07 -0400

    Initial commit with Restaurant model set up.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Servd::Application.config.secret_token = '3db71be40288f2939c4ee472f1a43a2f899863636e4bb9a8eb998f177ce74085b85df599d46337b0b8b94412d43ab42fdbd0284a1e0654810fb37b58e7f5548f'
